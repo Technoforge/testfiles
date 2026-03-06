524bc353185f2056c06c12fa48c6dbd17348009d
Technoforge <96360355+Technoforge@users.noreply.github.com>
2026-03-07 00:51:56 +1100

    Add files via upload

---
+++ Install-Wolpertinger.bat
@@ -0,0 +1,4 @@
+@echo off
+set "ZIP=%~dp0Wolpertinger-Installer.zip"
+set "TEMPINST=%TEMP%\Wolpertinger-Install"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "Expand-Archive -LiteralPath '%ZIP%' -DestinationPath '%TEMPINST%' -Force; Start-Process -FilePath '%TEMPINST%\install_launcher.exe' -Verb RunAs -Wait"
